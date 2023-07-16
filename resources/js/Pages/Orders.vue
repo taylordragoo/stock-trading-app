751a53a6364510669b38443d5dbecb98c3aba5b2
nosyke <greatbeyondstudios@gmail.com>
2023-07-16 14:21:50 -0400

    Hotfix for buttons on list views

--- resources/js/Pages/Orders.vue
+++ resources/js/Pages/Orders.vue
@@ -117,7 +117,7 @@ export default defineComponent({
                                     <span class="font-semibold leading-tight text-xs text-gray-800">{{ formattedDate(t.created_at) }}</span>
                                 </td>
                                 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                    <button @click="activate('AAPL')" type="button" class="inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md">
+                                    <button @click="activate(t.stock_id)" type="button" class="inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md">
                                         View
                                     </button>
                                 </td>
--- resources/js/Pages/Watchlist.vue
+++ resources/js/Pages/Watchlist.vue
@@ -207,7 +207,7 @@ export default defineComponent({
                                     <p class="mb-0 font-semibold leading-tight text-xs text-gray-800">{{ formatPercentage(t.todaysChangePerc) }}</p>
                                 </td>
                                 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent">
-                                    <button @click="activate('AAPL')" type="button" class="inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md">
+                                    <button @click="activate(t.item)" type="button" class="inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-blue-500 to-violet-500 leading-normal text-xs ease-in tracking-tight-rem shadow-xs bg-150 bg-x-25 hover:-translate-y-px active:opacity-85 hover:shadow-md">
                                         View
                                     </button>
                                 </td>
